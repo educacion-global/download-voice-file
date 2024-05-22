6c04756d71a22e3e491abd0da01f52453cca2512
Educación Global <73619263+educacion-global@users.noreply.github.com>
2024-05-22 09:29:28 -0600

    Update downloadAACFileWithToken.js v2

--- downloadAACFileWithToken.js
+++ downloadAACFileWithToken.js
@@ -1,16 +1,21 @@
-function downloadAACFileWithToken(url, token, output = "file") {
-    fetch(url, {
+function downloadAACFileWithToken(url, params, token, output = "audiofile") {
+    const queryString = new URLSearchParams(params).toString();
+    const fullUrl = `${url}?${queryString}`;
+    fetch(fullUrl, {
         headers: {
             'Authorization': 'Bearer ' + token
         }
     })
     .then(response => response.blob())
     .then(blob => {
-        const url = URL.createObjectURL(blob);
+        const objectUrl = URL.createObjectURL(blob);
         const a = document.createElement('a');
-        a.href = url;
+        a.href = objectUrl;
         a.download = `${output}.aac`;
         a.click();
-        URL.revokeObjectURL(url);
+        URL.revokeObjectURL(objectUrl);
+    })
+    .catch(error => {
+        console.error('Error downloading file:', error);
     });
 }
